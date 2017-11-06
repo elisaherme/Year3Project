ac72c1f4b34166cfaa0c62ab14b9d4ae5f5e6c0b
Elisa Sierra <elisaherme@gmail.com>
2017-11-06 13:09:46 +0000

    Add files via upload

---
+++ ArduinoCode.ino
@@ -0,0 +1,26 @@
+int ethanol_sensor = 1;
+int temp_sensor = 2;
+int heat_element = 3;
+
+float tempValue = 0.0;
+float ethanolValue = 0.0;
+
+void setup() {
+  Serial.begin(9600);// put your setup code here, to run once:
+
+}
+
+void loop() {
+  ethanolValue = analogRead(ethanol_sensor); //Process value from the sensor into actual ethanol value
+  //Save value - send through Bluetooth
+  Serial.println("Ethanol Value: " + ethanolValue);
+  tempValue = analogRead(temp_sensor); //Process value from the sensor into actual temp
+  Serial.println("Temp Value: " + tempValue)// Print to screen for testing but not for final product
+  if (tempValue > 40){
+    turn heater OFF;
+  }
+  else{
+    turn heater ON;
+  }
+  delay(1);
+}
