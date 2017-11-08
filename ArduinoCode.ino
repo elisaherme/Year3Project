5677389c93c3b21b9daf779c259a38c8cbb0e4b9
elisa <elisaherme@gmail.com>
2017-11-08 19:22:01 +0000

    temp conversion outlined

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -17,6 +17,7 @@ void setup() {
 }
 
 void loop() {
+  // analogRead returns value 0-1023 where the input voltage is 0-5V
   ethanolReading = analogRead(ethanol_sensor);
   // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
   //ethanolVoltage = map(ethanolReading, 0, 1023, 0, 5000); //Change values depending on the sensor
@@ -26,10 +27,11 @@ void loop() {
   Serial.println("Ethanol Value: " + ethanolValue);
 
   tempReading = analogRead(temp_sensor); //Process value from the sensor into actual temp
-  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
-  //tempVoltage = map(tempReading, 0, 1023, 0, 5000); //Change values depending on the sensor
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 3.3V (= 3300mV)
+  //tempVoltage = map(tempReading, 0, 1023, 0, 3300); //Change values depending on the sensor
   //Process value from the sensor into actual temperature
   //tempValue =
+  // Centigrade temperature = [(analog voltage in mV) - 500] / 10
   Serial.println("Temp Value: " + tempValue); // Print temp to screen for testing but not for final product
 
   if (tempValue >= 40){
