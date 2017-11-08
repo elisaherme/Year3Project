67c4901c0b1fcf9e7d04020b0cfe4159cacd79c3
elisa <elisaherme@gmail.com>
2017-11-08 18:48:55 +0000

    map voltage from sensors and outline to process the values

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -2,6 +2,12 @@ int ethanol_sensor = 1;
 int temp_sensor = 2;
 int heat_element = 3;
 
+float ethanolVoltage = 0;
+float tempVoltage = 0;
+
+float ethanolReading = 0;
+float tempReading = 0;
+
 float tempValue = 0.0;
 float ethanolValue = 0.0;
 
@@ -11,16 +17,27 @@ void setup() {
 }
 
 void loop() {
-  ethanolValue = analogRead(ethanol_sensor); //Process value from the sensor into actual ethanol value
+  ethanolReading = analogRead(ethanol_sensor);
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
+  //ethanolVoltage = map(ethanolReading, 0, 1023, 0, 5000); //Change values depending on the sensor
+  //Process value from the sensor into actual ethanol value
+  //ethanolValue =
   //Save value - send through Bluetooth
   Serial.println("Ethanol Value: " + ethanolValue);
-  tempValue = analogRead(temp_sensor); //Process value from the sensor into actual temp
+
+  tempReading = analogRead(temp_sensor); //Process value from the sensor into actual temp
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
+  //tempVoltage = map(tempReading, 0, 1023, 0, 5000); //Change values depending on the sensor
+  //Process value from the sensor into actual temperature
+  //tempValue =
   Serial.println("Temp Value: " + tempValue); // Print temp to screen for testing but not for final product
+
   if (tempValue >= 40){
     digitalWrite(heat_element, LOW); //turn heater OFF;
   }
   else{
     digitalWrite(heat_element, HIGH); //turn heater ON;
   }
+
   delay(1000); //It's in milliseconds
 }
