1c6971b6937755e707555dcc6027f1f2fbddb32b
elisa <elisaherme@gmail.com>
2017-11-08 16:51:26 +0000

    set heating element as outpu pin

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -6,8 +6,8 @@ float tempValue = 0.0;
 float ethanolValue = 0.0;
 
 void setup() {
-  Serial.begin(9600);// put your setup code here, to run once:
-
+  Serial.begin(9600); // put your setup code here, to run once:
+  pinMode(heat_element, OUTPUT); //sets the digital pin of the heating element as an output
 }
 
 void loop() {
@@ -15,12 +15,12 @@ void loop() {
   //Save value - send through Bluetooth
   Serial.println("Ethanol Value: " + ethanolValue);
   tempValue = analogRead(temp_sensor); //Process value from the sensor into actual temp
-  Serial.println("Temp Value: " + tempValue)// Print to screen for testing but not for final product
+  Serial.println("Temp Value: " + tempValue); // Print temp to screen for testing but not for final product
   if (tempValue > 40){
-    //turn heater OFF;
+    digitalWrite(heat_element, LOW); //turn heater OFF;
   }
   else{
-    //turn heater ON;
+    digitalWrite(heat_element, HIGH); //turn heater ON;
   }
   delay(1);
 }
