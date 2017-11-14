8cf3e3bc6b5781905999085941c255506f93076a
Elisa Sierra <elisaherme@gmail.com>
2017-11-14 14:35:11 +0000

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,10 +1,21 @@
+// First we include the libraries
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 2 
+
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
 int ethanol_sensor = 1;
-int temp_sensor = 2;
 int heat_element = 3;
 int threshold = 40; //sweat prodcing threshold temperature
 
 float ethanolVoltage = 0.0;
-float tempVoltage = 0.0;
 
 float ethanolReading = 0.0;
 float tempReading = 0.0;
@@ -15,6 +26,7 @@ float ethanolValue = 0.0;
 void setup() {
   Serial.begin(9600); //put your setup code here, to run once:
   pinMode(heat_element, OUTPUT); //sets the digital pin of the heating element as an output
+  sensors.begin();
 }
 
 void loop() {
@@ -27,12 +39,12 @@ void loop() {
   //Save value - send through Bluetooth
   Serial.println("Ethanol Value: " + ethanolValue);
 
-  tempReading = analogRead(temp_sensor); //Process value from the sensor into actual temp
-  //analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
-  tempVoltage = map(tempReading, 0, 1023, 0, 5000); //Change values depending on the sensor
-  //Process value from the sensor into actual temperature
-  //tempValue = temp formula (based on sensor)
-  //Centigrade temperature = [(analog voltage in mV) - 500] / 10
+  sensors.requestTemperatures(); // Send the command to get temperature readings
+  //Put temperate sensor into digital pin 2
+  tempValue = sensors.getTempCByIndex(0);// Why "byIndex"?  
+   // You can have more than one DS18B20 on the same bus.  
+   // 0 refers to the first IC on the wire 
+  
   Serial.println("Temp Value: " + tempValue); // Print temp to screen for testing but not for final product
 
   if (tempValue >= threshold){
