d9e19925c738589354213418811c7e9627a36369
elisa <elisaherme@gmail.com>
2018-02-07 12:42:51 +0000

    Change to analogWrite for the mosfet with pulse modualtion

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,16 +1,16 @@
 
 
 // First we include the libraries
-#include <OneWire.h> 
+#include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into pin 2 on the Arduino 
-#define ONE_WIRE_BUS 2 
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
 
-// Setup a oneWire instance to communicate with any OneWire devices  
-// (not just Maxim/Dallas temperature ICs) 
-OneWire oneWire(ONE_WIRE_BUS); 
-// Pass our oneWire reference to Dallas Temperature. 
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 int ethanol_sensor = A0;
@@ -34,11 +34,12 @@ void setup() {
 }
 
 void loop() {
+
   //analogRead returns value 0-1023 where the input voltage is 0-5V
   ethanolReading = analogRead(ethanol_sensor);
   //analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
   ethanolVoltage = map(ethanolReading, 0, 1023, 0, 5000); //Change values depending on the sensor
-  
+
   //In percentage
   ethanolValue = 10^((ethanolVoltage - 3993)/245.9); //Sensor 1
   Serial.println("Ethanol Value: ");
@@ -47,19 +48,23 @@ void loop() {
 
   sensors.requestTemperatures(); // Send the command to get temperature readings
   //Put temperate sensor into digital pin 2
-  tempValue = sensors.getTempCByIndex(0);// Why "byIndex"?  
-   // You can have more than one DS18B20 on the same bus.  
-   // 0 refers to the first IC on the wire 
-  
+  tempValue = sensors.getTempCByIndex(0);// Why "byIndex"?
+   // You can have more than one DS18B20 on the same bus.
+   // 0 refers to the first IC on the wire
+
   Serial.println("Temp Value: "); // Print temp to screen for testing but not for final product
   Serial.println(tempValue);
   Serial.println("/n");
-  
-  if (tempValue >= threshold){
+
+
+  if (tempValue <= 32){
+    analogWrite(heat_element, 255); //turn heater ON with no pulse so it heats fast
+  }
+  else if (tempValue >= threshold){
     digitalWrite(heat_element, LOW); //turn heater OFF;
   }
   else{
-    digitalWrite(heat_element, HIGH); //turn heater ON;
+    analogWrite(heat_element, 51); //turn heater ON;
   }
 
   delay(10000); //It's in milliseconds
