07d75373f28a07b5916a41ea5484fbd9399792d9
Elisa Sierra <elisaherme@gmail.com>
2018-05-28 21:05:58 +0100

    Update CodeBluno.ino

--- ArduinoCode/CodeBluno.ino
+++ ArduinoCode/CodeBluno.ino
@@ -19,11 +19,11 @@ int threshold = 40; //sweat prodcing threshold temperature
 float ethanolVoltage = 0.0;
 float sensorResistance = 0.0;
 
-float ethanolReading = 0.0;
+float ethanolReading = 4.0;
 float tempReading = 0.0;
 
 float tempValue = 0.0;
-double ethanolValue = 0.0; //measured in ppm
+double ethanolValue = 3.0; //measured in ppm
 
 void setup() {
   Serial.begin(115200); //put your setup code here, to run once:
@@ -32,21 +32,24 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("#");
+  
   //analogRead returns value 0-1023 where the input voltage is 0-5V
   ethanolReading = analogRead(ethanol_sensor);
   //analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
   ethanolVoltage = map(ethanolReading, 0, 1023, 0, 5000); //Change values depending on the sensor
 
+  Serial.print("ethanol value = ");
   //In percentage
-  ethanolValue = pow(10,(ethanolVoltage - 3993)/245.9); //Sensor 1
-  Serial.print("|");
-
+  ethanolValue = pow(10,(ethanolVoltage - 3578.9)/328.0); //Sensor 2
+  Serial.println(ethanolValue);
+  
+  Serial.print("temp = ");
   sensors.requestTemperatures(); // Send the command to get temperature readings
   //Put temperate sensor into digital pin 2
   tempValue = sensors.getTempCByIndex(0);// Why "byIndex"?
    // You can have more than one DS18B20 on the same bus.
    // 0 refers to the first IC on the wire
+  Serial.println(tempValue);
 
   if (tempValue >= threshold){
     digitalWrite(heat_element, LOW); //turn heater OFF;
@@ -55,5 +58,5 @@ void loop() {
     digitalWrite(heat_element, HIGH); //turn heater ON
   }
 
-  delay(10000); //It's in milliseconds
+  delay(3000); //It's in milliseconds
 }
